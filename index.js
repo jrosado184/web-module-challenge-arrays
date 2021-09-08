12a7e85504ffe919fed03287bd6a0d7ac28fa194
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2021-09-08 17:12:00 -0400

    finshed 1-5

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const originalFlavors = [
   "Vanilla Burnt Almond"
 ]
 
+
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: Copy the Array! 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 We have an array called originalFlavors with 31 flavors (see above).  In these tasks, we will be reading and writing data to this array.  
 With all of these changes going on, we don't want to lose track of the actual, original 31 flavors.  So we need to copy the original array!
@@ -45,9 +46,16 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-  /*your code here*/
-}    
+function copy(arr){
+  const newArray = [...arr]
+  return newArray;
+}
+  copy(originalFlavors)
+  console.log(copy(originalFlavors));
+
+
+
+
 
 
 
@@ -64,9 +72,15 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
- /*your code here*/
-}
+function is31Flavors(originalFlavors){
+  if(originalFlavors.length === 31){
+   return true;
+  }else{
+    return false;
+  }
+ }
+ 
+     
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -81,9 +95,11 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
- /*your code here*/
-}
+function addFlavor(originalFlavors, newFlavor){
+originalFlavors.unshift(newFlavor);
+return originalFlavors
+} 
+console.log(addFlavor(originalFlavors, "cookie"));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -97,10 +113,13 @@ Use the removeLastFlavor function below to do the following:
   For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+function removeLastFlavor(originalFlavors){
+ const r = [...originalFlavors]
+ r.pop();
+ return r;
 }
 
+console.log(removeLastFlavor(originalFlavors));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
